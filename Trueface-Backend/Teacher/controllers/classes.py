a4154413ac09aa859f6f0ed2081b40de4f8fbde6
Ahmadalmuhidat <ahmad.almuhidat@gmail.com>
2025-08-13 13:52:41 +0300

    get classes based on today

--- Trueface-Backend/Teacher/controllers/classes.py
+++ Trueface-Backend/Teacher/controllers/classes.py
@@ -1,25 +1,40 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from datetime import date
 from ..utils.database import Database
 from ..helper import json_web_token
 
 @csrf_exempt
 def get_current_teacher_classes(request):
   if request.method == "GET":
-    current_teacher = json_web_token.validate_token(request.GET.get("current_teacher")).get('user_id')
-    data = [current_teacher]
+    current_teacher = json_web_token.validate_token(
+      request.GET.get("current_teacher")
+    ).get('user_id')
+
+    today = date.today()
+    data = [current_teacher, today]
+
     query = '''
       SELECT
-        ID,
-        SubjectArea,
-        StartTime,
-        EndTime
+        Classes.ID,
+        Classes.SubjectArea,
+        Classes.StartTime,
+        Classes.EndTime
       FROM
         Classes
+      JOIN
+        ClassStudentRelation
+      ON
+        Classes.ID = ClassStudentRelation.Class
       WHERE
-        Instructor = %s
+        Classes.Instructor = %s
+      AND
+        ClassStudentRelation.Day = %s
     '''
-    return JsonResponse({"status_code": 200, "data": Database.ExecuteGetQuery(query, data)})
+    classes = Database.ExecuteGetQuery(query, data)
+
+    return JsonResponse({"status_code": 200, "data": classes})
+
   return JsonResponse({
     "status_code": 405,
     "error": "Method not allowed"
